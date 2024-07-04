471c7c17ac3f2c20a93e4b8494334baa147cc315
Botirjon Ergashov <139768692+TalantDeveloper@users.noreply.github.com>
2024-07-04 18:24:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token("7298822162:AAF6PEEkwpYhb6ikJHuT6U37C_lpc2CjXgo").build()
+app = ApplicationBuilder().token("").build()
 
 app.add_handler(CommandHandler("start", hello))
 
