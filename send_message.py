02f720343bae1e04e812702ba3dcb6d7e9bcee3d
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2025-01-09 23:19:56 +0500

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -7,18 +7,20 @@ base_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
 chat_ids = ['-1002198877314', '-1002152186194']
 
-parameters = {'chat_id': chat_ids[0], 'text': 'Assalomu alaykum bugun 5-iyul'}
+parameters = {'chat_id': chat_ids[0], 'text': 'Assalomu alaykum bugun 3.09.2024'}
 
-# response = requests.get(base_url, data=parameters)
-# print(response.text)
+response = requests.get(base_url, data=parameters)
+print(response.text)
 
-# # Ko'p habar bir vaqtda yuborish
-# texts = ['Assalomu alaykum', 'Bugun osmon ochiq boladi', 'harorat 35 gradus issiq bolishi kutilmoqda',
-#          'kun davomida koproq suv iching'
-#          ]
-# for text in texts:
-#     time.sleep(5)
-#     parameters['text'] = text
-#     parameters['chat_id'] = chat_ids[1]
-#     response = requests.get(base_url, data=parameters)
-#     print(response.text)
+# Ko'p habar bir vaqtda yuborish
+texts = ['Assalomu alaykum',
+         'Bugun osmon ochiq boladi',
+         'harorat 35 gradus issiq bolishi kutilmoqda',
+         'kun davomida koproq suv iching'
+         ]
+for text in texts:
+    time.sleep(5)
+    parameters['text'] = text
+    parameters['chat_id'] = chat_ids[1]
+    response = requests.get(base_url, data=parameters)
+    print(response.text)
