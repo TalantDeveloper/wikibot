183db4e38f542199640755f1563a395c684abecf
Botirjon Ergashov <139768692+TalantDeveloper@users.noreply.github.com>
2025-03-15 16:46:44 +0500

    Update bot_api.py

--- bot_api.py
+++ bot_api.py
@@ -1,7 +1,8 @@
 import requests
-import key
 
-TOKEN = key.TOKEN
+# import key
+
+TOKEN = "7652126290:AAGX-UjXsVVAs_Ah0sHgjm3_BzhL9iaRB5A"
 
 # rest = requests.get(f"https://api.telegram.org/bot{TOKEN}/getUpdates")
 # print(rest.text)
@@ -19,18 +20,20 @@ TOKEN = key.TOKEN
 base_url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 
 parameters = {
-    'offset': '-1002481531999',
+    'offset': '1834494100',
     'limit': '100',
 }
-response = requests.get(base_url, data=parameters)
-# print(response.text)
-first = response.json()
+
+while True:
+    rest = requests.get(f"https://api.telegram.org/bot{TOKEN}/getUpdates")
+    print(rest.text)
+# first = response.json()
 # print(first)
 
-second = first['result']
-length = len(second)
-print(second[length - 1])
-print(second[length - 1]['channel_post']['message_id'])
+# second = first['result']
+# length = len(second)
+# print(second[length - 1])
+# print(second[length - 1]['channel_post']['message_id'])
 # print(second.last())
 
 
